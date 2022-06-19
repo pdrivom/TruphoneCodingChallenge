18fb31d362533baa6b143b11680df637fedfcc80
Pedro Marques <pedro.ivo996@gmail.com>
2022-06-19 15:46:55 +0100

    bug fixed in tests

--- src/api/tests/test_api.py
+++ src/api/tests/test_api.py
@@ -8,37 +8,30 @@ client = TestClient(app)
 
 
 def test_read_organization():
-    # since this response is known, verification can be made
-    response = client.get("/api/v1/organization/x00g8")
+    org_id = 'x00g8'
+    response = client.get(f"/api/v1/organization/{org_id}")
     assert response.status_code == 200
-    assert response.json() == {
-        "id": "1",
-        "org_id": "x00g8",
-    }
+    assert response.json()["org_id"] == org_id
 
 def test_read_inexistent_organization():
-    # since this response is known, verification can be made
-    response = client.get("/api/v1/organization/x30g5")
+    org_id = 'x30g5'
+    response = client.get(f"/api/v1/organization/{org_id}")
     assert response.status_code == 404
-    assert response.json() == {"detail": "No organization x30g5 found!"}
+    assert response.json() == {"detail": f"No organization {org_id} found!"}
 
 def test_read_simcard():
-    # since this response is known, verification can be made
-    response = client.get("/api/v1/organization/x00g8")
+    sim_card_id = '89440001'
+    response = client.get(f"/api/v1/simcard/{sim_card_id}")
     assert response.status_code == 200
-    assert response.json() == {
-        "id": "1",
-        "org_id": "x00g8",
-    }
+    assert response.json()["sim_card_id"] == sim_card_id
+
 
 def test_read_inexistent_simcard():
-    # since this response is known, verification can be made
     response = client.get("/api/v1/simcard/89440000")
     assert response.status_code == 404
     assert response.json() == {"detail": "No simcard 89440000 found!"}
 
 def test_read_simcard_usage_parameters():
-    # since this response is known, verification can be made
     page = 1
     size = 15
     response = client.get(f"api/v1/simcard/89440001/usage?start=2020-02-01&end=2020-02-01&every=1day&page={page}&size={size}")
@@ -49,7 +42,6 @@ def test_read_simcard_usage_parameters():
     assert response.json()["size"] == size
 
 def test_read_simcard_usage_missing_page():
-    # since this response is known, verification can be made
     size = 15
     response = client.get(f"api/v1/simcard/89440001/usage?start=2020-02-01&end=2020-02-01&every=1day&size={size}")
     assert response.status_code == 200
@@ -58,7 +50,6 @@ def test_read_simcard_usage_missing_page():
     assert response.json()["size"] == size
 
 def test_read_simcard_usage_missing_page_and_size():
-    # since this response is known, verification can be made
     response = client.get(f"api/v1/simcard/89440001/usage?start=2020-02-01&end=2020-02-01&every=1day")
     assert response.status_code == 200
     assert "items" in response.json()
@@ -67,30 +58,25 @@ def test_read_simcard_usage_missing_page_and_size():
     assert response.json()["size"] == 50
 
 def test_read_simcard_usage_bad_every():
-    # since this response is known, verification can be made
     every = 'hello'
     response = client.get(f"api/v1/simcard/89440001/usage?start=2020-02-01&end=2020-02-01&every={every}")
     assert response.status_code == 422
     assert response.json() == {"detail": f"Query parameter 'every' cannot be {every}. (ex: 1day / 1hour)"}
 
 def test_read_simcard_usage_bad_every():
-    # since this response is known, verification can be made
     every = 'hello'
     response = client.get(f"api/v1/simcard/89440001/usage?start=2020-02-01&end=2020-02-01&every={every}")
     assert response.status_code == 422
     assert response.json() == {"detail": f"Query parameter 'every' cannot be {every}. (ex: 1day / 1hour)"}
 
 def test_read_simcard_usage_missing_every():
-    # since this response is known, verification can be made
     response = client.get("api/v1/simcard/89440001/usage?start=2020-02-01&end=2020-02-01")
     assert response.status_code == 422
 
 def test_read_simcard_usage_bad_end():
-    # since this response is known, verification can be made
     response = client.get("api/v1/simcard/89440001/usage?start=2020-01-02&end=2020-02-00")
     assert response.status_code == 422
 
 def test_read_simcard_usage_missing_dates():
-    # since this response is known, verification can be made
     response = client.get("api/v1/simcard/89440001/usage")
     assert response.status_code == 422
